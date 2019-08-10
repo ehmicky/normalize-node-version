b8a2f346c4f66b358c40e7cf176256bff0a00edf
ehmicky <ehmicky@gmail.com>
2019-08-10 10:00:00 +0200

    Add test

--- test/main.js
+++ test/main.js
@@ -15,6 +15,13 @@ test('Success', async t => {
   t.is(version, '4.9.1')
 })
 
+test('Twice in same process', async t => {
+  await normalizeNodeVersion('4')
+  const version = await normalizeNodeVersion('4')
+
+  t.is(version, '4.9.1')
+})
+
 each(['4.*', '<5'], ({ title }, versionRange) => {
   test(`Versions range | ${title}`, async t => {
     const version = await normalizeNodeVersion(versionRange)
