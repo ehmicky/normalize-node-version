a709380398e8f8769cdc502f76dab3b0ac742289
ehmicky <ehmicky@gmail.com>
2020-04-12 13:18:59 +0200

    Add empty line

--- src/cache.js
+++ src/cache.js
@@ -14,6 +14,7 @@ import writeFileAtomic from 'write-file-atomic'
 export const getCachedVersions = async function (versionRange) {
   const cacheFile = await getCacheFile()
   const cacheStat = await getCacheStat(cacheFile)
+
   const cachedVersions = await getFileCachedVersions({
     versionRange,
     cacheFile,
