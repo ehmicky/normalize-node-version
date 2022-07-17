df03d843c028b4fd3b516a4065853521f55e4f75
ehmicky <ehmicky@gmail.com>
2022-07-17 19:21:32 +0200

    Fix `filter-obj`

--- src/options.js
+++ src/options.js
@@ -1,10 +1,10 @@
-import filterObj from 'filter-obj'
+import { excludeKeys } from 'filter-obj'
 import { validate } from 'jest-validate'
 
 // Normalize options and assign default values
 export const getOpts = function (opts = {}) {
   validate(opts, { exampleConfig: EXAMPLE_OPTS })
-  const optsA = filterObj(opts, isDefined)
+  const optsA = excludeKeys(opts, isUndefined)
   const optsB = { ...DEFAULT_OPTS, ...optsA }
 
   const { fetch: fetchOpt, mirror } = optsB
@@ -22,6 +22,6 @@ const EXAMPLE_OPTS = {
   mirror: 'https://nodejs.org/dist',
 }
 
-const isDefined = function (key, value) {
-  return value !== undefined
+const isUndefined = function (key, value) {
+  return value === undefined
 }
