41470010dc1a66c9ef050390ecb9eeea4586bc61
ehmicky <ehmicky@gmail.com>
2022-12-04 23:19:54 +0100

    Refactoring

--- src/helpers/versions.test.js
+++
@@ -1,5 +0,0 @@
-// Test Node.js versions
-export const FULL_VERSION = '4.9.1'
-export const RANGES = ['4', '4.*', '<5']
-export const TOO_HIGH_VERSION = '90'
-export const INVALID_VERSION = 'not_a_valid_version'
--- src/main.test.js
+++ src/main.test.js
@@ -2,7 +2,29 @@ import test from 'ava'
 import normalizeNodeVersion from 'normalize-node-version'
 import { each } from 'test-each'
 
-import { FULL_VERSION, RANGES } from './helpers/versions.test.js'
+const FULL_VERSION = '4.9.1'
+const RANGES = ['4', '4.*', '<5']
+const TOO_HIGH_VERSION = '90'
+const INVALID_VERSION = 'not_a_valid_version'
+
+each(
+  [
+    {},
+    { versionRange: INVALID_VERSION },
+    { versionRange: TOO_HIGH_VERSION },
+    { opts: true },
+    {
+      versionRange: FULL_VERSION,
+      opts: { mirror: 'not_valid_url', fetch: true },
+    },
+    { opts: { fetch: 0 } },
+  ],
+  ({ title }, { versionRange, opts }) => {
+    test(`Invalid input | ${title}`, async (t) => {
+      await t.throwsAsync(normalizeNodeVersion(versionRange, opts))
+    })
+  },
+)
 
 each(RANGES, ({ title }, versionRange) => {
   test(`Resolves versions range | ${title}`, async (t) => {
